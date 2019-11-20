3299c929cf161209c49e123eb614ad85d6752f0a
davidlin <davidlindev@gmail.com>
2019-11-20 12:46:24 +0800

    add

--- class4.sql
+++ class4.sql
@@ -11,6 +11,7 @@
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
@@ -18,9 +19,7 @@
 -- Current Database: `class4`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `class4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `class4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `class4`;
 
@@ -45,7 +44,7 @@ CREATE TABLE `institute` (
 
 LOCK TABLES `institute` WRITE;
 /*!40000 ALTER TABLE `institute` DISABLE KEYS */;
-INSERT INTO `institute` VALUES ('01','IDL','baidu campus'),('02','cv lab','silicon valley');
+INSERT INTO `institute` VALUES ('01','IDL','baidu campus'),('02','cv lab','silicon valley'),('03','huawei','shenzhen'),('04','zhongxing','beijing');
 /*!40000 ALTER TABLE `institute` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +64,7 @@ CREATE TABLE `involvement` (
   KEY `researcher` (`researcher`),
   CONSTRAINT `involvement_in_project` FOREIGN KEY (`project`) REFERENCES `project` (`id`),
   CONSTRAINT `researcher_in_project` FOREIGN KEY (`researcher`) REFERENCES `researcher` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +73,7 @@ CREATE TABLE `involvement` (
 
 LOCK TABLES `involvement` WRITE;
 /*!40000 ALTER TABLE `involvement` DISABLE KEYS */;
-INSERT INTO `involvement` VALUES (1,'01','01'),(2,'02','01'),(3,'01','02'),(4,'02','03');
+INSERT INTO `involvement` VALUES (1,'01','01'),(2,'02','01'),(3,'01','02'),(4,'02','03'),(5,'02','04'),(6,'03','04'),(7,'x01','03'),(8,'x01','04'),(9,'x01','06'),(10,'x02','01'),(11,'04','08'),(12,'04','06'),(14,'x02','11'),(15,'x01','10'),(16,'x01','12'),(17,'x02','12'),(18,'01','12');
 /*!40000 ALTER TABLE `involvement` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,7 +92,7 @@ CREATE TABLE `project` (
   PRIMARY KEY (`id`),
   KEY `institute` (`institute`),
   CONSTRAINT `institute_in_project` FOREIGN KEY (`institute`) REFERENCES `institute` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +101,7 @@ CREATE TABLE `project` (
 
 LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
-INSERT INTO `project` VALUES ('01','liver segmentation',1000.00,'01'),('02','lung noudle segmentation',4321.98,'02');
+INSERT INTO `project` VALUES ('01','liver segmentation',1000.00,'01'),('02','lung noudle segmentation',4321.98,'02'),('03','face recogenition',5321.11,'02'),('04','paddle paddle',61374.32,'01'),('x01','X01',2222.32,'04'),('x02','X02',1412.34,'03');
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,16 +131,17 @@ CREATE TABLE `researcher` (
 
 LOCK TABLES `researcher` WRITE;
 /*!40000 ALTER TABLE `researcher` DISABLE KEYS */;
-INSERT INTO `researcher` VALUES ('01','hanlin','male',20,'rd','01'),('02','robin li','male',45,'instructor','01'),('03','haifeng wang','male',40,'instructor','02');
+INSERT INTO `researcher` VALUES ('01','hanlin','male',20,'rd','01'),('02','robin li','male',45,'instructor','01'),('03','haifeng wang','male',40,'instructor','02'),('04','Li na','female',20,'rd','02'),('05','mdzz','male',8,'cleaner','01'),('06','zuckberg','male',30,'ceo','03'),('07','zhangrui','male',42,'rd','03'),('08','zjp','male',6,'leader','03'),('09','mlq','female',15,'pm','03'),('10','pengge','female',47,'cleaner','04'),('11','liujian','male',18,'rd','04'),('12','Ma fei','male',34,'rd','02');
 /*!40000 ALTER TABLE `researcher` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-20  9:42:18
+-- Dump completed on 2019-11-20 12:45:21
